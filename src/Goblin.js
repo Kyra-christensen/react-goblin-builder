ce8c9ff2e87ccbf43e6a49a75f2a997cc92b617e
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-02 14:49:33 -0800

    created goblinList function that maps through the goblins, and created a goblin function that renders a single goblin

--- src/Goblin.js
+++ src/Goblin.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-export default function Goblin(props) {
+export default function Goblin({ 
+  name,
+  hp,
+  color,
+  id,
+  handleDeleteGoblin
+}) {
   return (
     // be sure you take a look at this component i'm handing you and figure out what props it will need to work correctly.
     <div 
       className='goblin' 
-      onClick={() => props.handleDeleteGoblin && props.handleDeleteGoblin(props.goblin.id)}>
-      <h3>{props.goblin.name}</h3>  
-      <img src="goblin.png" style={{ backgroundColor: props.goblin.color }} />
-      <p>{props.goblin.hp} HP</p>
+      onClick={() => handleDeleteGoblin && handleDeleteGoblin({ id })}>
+      <h3>{name}</h3>  
+      <img src="goblin.png" style={{ backgroundColor: { color } }} />
+      <p>{hp} HP</p>
     </div>
   );
 }
